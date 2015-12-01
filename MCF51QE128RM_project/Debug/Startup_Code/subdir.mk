2bd14f4d7d4aded257950cc99af918c6ee6826b2
Matheus <matheusdalmago10@hotmail.com>
2015-12-01 17:00:17 -0200

    Removing unnecessary files

--- MCF51QE128RM_project/Debug/Startup_Code/subdir.mk
+++ MCF51QE128RM_project/Debug/Startup_Code/subdir.mk
@@ -27,14 +27,14 @@ C_DEPS += \
 Startup_Code/%.o: ../Startup_Code/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: Coldfire C Compiler'
-	m68k-elf-gcc -mcpu=51 -g3 -O0 -ffunction-sections -fdata-sections -DDEBUG_BUILD -I"C:/Users/Matheus Dal Mago/Documents/GitHub/brtos/MCF51QE128RM_project/Sources" -I"C:/Users/Matheus Dal Mago/Documents/GitHub/brtos/MCF51QE128RM_project/Project_Headers" -I"C:\Users\Matheus Dal Mago\Documents\GitHub\brtos\brtos\includes" -I"C:\Users\Matheus Dal Mago\Documents\GitHub\brtos\hal\GCC_CFV1" -I"C:\Users\Matheus Dal Mago\Documents\GitHub\brtos\hal\MemoryAllocation" -Wall -std=c99 -c -fmessage-length=0 -MT"$@" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)"  -o "$@" $<
+	m68k-elf-gcc -mcpu=51 -g3 -O0 -ffunction-sections -fdata-sections -DDEBUG_BUILD -I"C:/Users/Matheus Dal Mago/Documents/GitHub/brtos/MCF51QE128RM_project/Sources" -I"C:/Users/Matheus Dal Mago/Documents/GitHub/brtos/MCF51QE128RM_project/Project_Headers" -I"C:/Users/Matheus Dal Mago/Documents/GitHub/brtos/MCF51QE128RM_project/../brtos/includes" -I"C:/Users/Matheus Dal Mago/Documents/GitHub/brtos/MCF51QE128RM_project/../hal/GCC_CFV1" -I"C:/Users/Matheus Dal Mago/Documents/GitHub/brtos/MCF51QE128RM_project/../hal/MemoryAllocation" -Wall -std=c99 -c -fmessage-length=0 -MT"$@" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)"  -o "$@" $<
 	@echo 'Finished building: $<'
 	@echo ' '
 
 Startup_Code/%.o: ../Startup_Code/%.S
 	@echo 'Building file: $<'
 	@echo 'Invoking: Coldfire Assembler'
-	m68k-elf-gcc -mcpu=51 -g3 -O0 -ffunction-sections -fdata-sections -x assembler-with-cpp -DDEBUG_BUILD -I"C:/Users/Matheus Dal Mago/Documents/GitHub/brtos/MCF51QE128RM_project/Project_Headers" -I"C:\Users\Matheus Dal Mago\Documents\GitHub\brtos\brtos\includes" -I"C:\Users\Matheus Dal Mago\Documents\GitHub\brtos\hal\GCC_CFV1" -I"C:\Users\Matheus Dal Mago\Documents\GitHub\brtos\hal\MemoryAllocation" -Wall -Wextra -c -fmessage-length=0  -o "$@" $<
+	m68k-elf-gcc -mcpu=51 -g3 -O0 -ffunction-sections -fdata-sections -x assembler-with-cpp -DDEBUG_BUILD -I"C:/Users/Matheus Dal Mago/Documents/GitHub/brtos/MCF51QE128RM_project/Project_Headers" -I"C:/Users/Matheus Dal Mago/Documents/GitHub/brtos/MCF51QE128RM_project/../brtos/includes" -I"C:/Users/Matheus Dal Mago/Documents/GitHub/brtos/MCF51QE128RM_project/../hal/GCC_CFV1" -I"C:/Users/Matheus Dal Mago/Documents/GitHub/brtos/MCF51QE128RM_project/../hal/MemoryAllocation" -Wall -Wextra -c -fmessage-length=0  -o "$@" $<
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- MCF51QE128RM_project/Debug/Startup_Code/vectors.d
+++
@@ -1,7 +0,0 @@
-Startup_Code/vectors.o Startup_Code/vectors.o: ../Startup_Code/vectors.c \
- C:/Users/Matheus\ Dal\ Mago/Documents/GitHub/brtos/MCF51QE128RM_project/Project_Headers/derivative.h \
- C:/Users/Matheus\ Dal\ Mago/Documents/GitHub/brtos/MCF51QE128RM_project/Project_Headers/MCF51QE128.h
-
-C:/Users/Matheus\ Dal\ Mago/Documents/GitHub/brtos/MCF51QE128RM_project/Project_Headers/derivative.h:
-
-C:/Users/Matheus\ Dal\ Mago/Documents/GitHub/brtos/MCF51QE128RM_project/Project_Headers/MCF51QE128.h:
